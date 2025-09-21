b353874d9d5112da8538aac0f29ec61872f035e7
Your Name <YOUR_EMAIL>
2025-09-21 14:36:16 +0530

    v

--- frontend/Task/src/pages/Auth/Login.jsx
+++ frontend/Task/src/pages/Auth/Login.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
-
+import React, { useState} from 'react'
+import AuthLayout from '../../componenets/layouts/AuthLayout'
+import {Link, useNavigate} from 'react-router-dom'
+import Input from '../../Inputs/Input'
+import { validateEmail } from '../../utils/helper'
+import axiosInstance from '../../utils/axiosIntance'
+// import { API_PATHS } from '../../utils/apiPath'
 const Login = () => {
+  const [email, setEmail] =useState("")
+  const [password, setPassword] =useState("")
+  const [error, setError] =useState("")
+  const navigate = useNavigate()
+  const handleLogin = async(e)=>{
+    e.preventDefault()
+    if (!validateEmail(email)) {
+  setError("Please enter a valid email address.");
+  return;
+}
+
+if (!password) {
+  setError("Please enter the password");
+  return;
+}
+
+setError("");
+//login api calling
+try{
+const response= await axiosInstance.post(API_PATHS.AUTH.LOGIN,{
+  email,
+  password
+});
+const {token,role}= response.data;
+if(token){
+  localStorage.setItem("token",token)
+  if(role==="admin"){
+    navigate("/admin/dashboard")
+  }else{
+   navigate("/user/dashboard")
+  }
+}
+}catch(err){
+  if(err.response&& err.response.data.message){
+    setError(err.response.data.message)
+
+  }else{
+    setError("Something went wrong. Please try again later.")
+  }
+  console.log("Login error", err);
+}
+  }
+
   return (
-    <div>Login</div>
+   <AuthLayout>
+    <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+     <h3 className='text-xl font-semibold text-black'>Welcome back</h3>
+     <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+      Please enter your details to log in
+     </p>
+     <form onSubmit={handleLogin}>
+      <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+         <Input
+          value={password}
+          onChange={(target) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+        {error && <p className='text-red-500 text-sm my-2'>{error}</p>}
+        <button className='btn-primary' onClick={handleLogin}>
+          LOGIN
+        </button>
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Don't have an account?{""}
+          <Link to="/signup" className='text-primary underline'>Signup
+          </Link>
+        </p>
+     </form>
+    </div>
+   </AuthLayout>
   )
 }
 
--- frontend/Task/src/pages/Auth/Signup.jsx
+++ frontend/Task/src/pages/Auth/Signup.jsx
@@ -1,12 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../componenets/layouts/AuthLayout'
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../Inputs/ProfilePhotoSelector';
+import Input from '../../Inputs/Input';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [adminInviteToken, setAdminInviteToken] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter your full name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError(""); // Clear the error if everything is valid
+  };
+
   return (
-    <div>
-      Signup
-    </div>
-  )
+    <AuthLayout>
+      <div className='lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Create Account</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Join us today by entering your details below
+        </p>
+        <form onSubmit={handleSubmit}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* You might want to add input fields here for fullName, email, and password */}
+            <Input
+            
+            value={fullName}
+            onChange={({target})=>{setFullName(target.value)}}
+            label={"Full Name"}
+            placeholder={"John"}
+            type={"text"}
+            />
+            <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+         <Input
+          value={password}
+          onChange={(target) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+        <Input
+          value={adminInviteToken}
+          onChange={(target) => setAdminInviteToken(target.value)}
+          label="Amin Invite Token"
+          placeholder="Min 6  Characters"
+          type="text"
+        />
+        </div>
+         {error && <p className='text-red-500 text-sm my-2'>{error}</p>}
+        <button className='btn-primary' onClick={handleSubmit}>
+          SIGN UP
+        </button>
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Have an account?{" "}
+          <Link to="/login" className='text-primary underline'>Login
+          </Link>
+          </p>
+          
+        </form>
+      </div>
+    </AuthLayout>
+  );
 }
 
-export default Signup
+export default Signup;
 
